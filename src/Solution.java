bb851bc5dcb28024d81b6916c4e1c4805bd735a6
Sachin Ravishika <SachinRa@lolctech.com>
2022-02-08 20:06:39 +0530

    #birthdayCakeCandles

--- src/Solution.java
+++ src/Solution.java
@@ -1,43 +1,60 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 
 class Result {
 
     /*
-     * Complete the 'gradingStudents' function below.
+     * Complete the 'birthdayCakeCandles' function below.
      *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts INTEGER_ARRAY grades as parameter.
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
      */
 
-    public static List<Integer> gradingStudents(List<Integer> grades) {
+    public static int birthdayCakeCandles(List<Integer> candles) {
         // Write your code here
-        List<Integer> response = new ArrayList<>();
-        for (Integer grade : grades) {
-            if (grade < 38 || grade % 5 <= 2) {
-                response.add(grade);
-            } else {
-                int add = 5 - (grade % 5);
-                response.add(grade + add);
+        int maxCount = 0, maxHeight = 0;
+        if (!candles.isEmpty()) {
+            for (int x : candles) {
+                if (x >= maxHeight) {
+                    if (maxHeight != x) {
+                        maxHeight = x;
+                        maxCount = 0;
+                    }
+                    maxCount++;
+                }
             }
         }
-        return response;
+        return maxCount;
     }
 }
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        List<Integer> grades = new ArrayList<>();
-//        grades.add(4);
-        grades.add(73);
-        grades.add(67);
-        grades.add(38);
-        grades.add(33);
+//        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
+        int candlesCount = 10;
 
-        List<Integer> result = Result.gradingStudents(grades);
+//        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+//                .map(Integer::parseInt)
+//                .collect(toList());
+        List<Integer> candles = Stream.of("18 90 90 13 90 75 90 8 90 43".replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
 
+        int result = Result.birthdayCakeCandles(candles);
         System.out.println(result);
+
+//        bufferedWriter.write(String.valueOf(result));
+//        bufferedWriter.newLine();
+//
+//        bufferedReader.close();
+//        bufferedWriter.close();
+
     }
 }
