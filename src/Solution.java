7495f7685f42c480b250a25bbd2333daca076ba4
Sachin Ravishika <SachinRa@lolctech.com>
2022-02-08 20:09:22 +0530

    #fizzBuzz

--- src/Solution.java
+++ src/Solution.java
@@ -1,60 +1,47 @@
 import java.io.*;
-import java.util.*;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
+import java.lang.Math;
 
 
 class Result {
-
+    private static final int constraint = (int) (2 * Math.pow(2, 10));
     /*
-     * Complete the 'birthdayCakeCandles' function below.
+     * Complete the 'fizzBuzz' function below.
      *
-     * The function is expected to return an INTEGER.
-     * The function accepts INTEGER_ARRAY candles as parameter.
+     * The function accepts INTEGER n as parameter.
      */
 
-    public static int birthdayCakeCandles(List<Integer> candles) {
+    public static void fizzBuzz(int n) {
         // Write your code here
-        int maxCount = 0, maxHeight = 0;
-        if (!candles.isEmpty()) {
-            for (int x : candles) {
-                if (x >= maxHeight) {
-                    if (maxHeight != x) {
-                        maxHeight = x;
-                        maxCount = 0;
-                    }
-                    maxCount++;
+        String result;
+        for (int i = 1; i <= n; i++) {
+            result = null;
+            if (i % 3 == 0) {
+                result = "Fizz";
+            }
+            if (i % 5 == 0) {
+                if (result == null) {
+                    result = "Buzz";
+                } else {
+                    result += "Buzz";
                 }
             }
+            if (result == null) {
+                result = String.valueOf(i);
+            }
+            System.out.println(result);
         }
-        return maxCount;
     }
+
 }
 
 public class Solution {
-    public static void main(String[] args) {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-//        int candlesCount = Integer.parseInt(bufferedReader.readLine().trim());
-        int candlesCount = 10;
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
 
-//        List<Integer> candles = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-//                .map(Integer::parseInt)
-//                .collect(toList());
-        List<Integer> candles = Stream.of("18 90 90 13 90 75 90 8 90 43".replaceAll("\\s+$", "").split(" "))
-                .map(Integer::parseInt)
-                .collect(toList());
+        Result.fizzBuzz(n);
 
-        int result = Result.birthdayCakeCandles(candles);
-        System.out.println(result);
-
-//        bufferedWriter.write(String.valueOf(result));
-//        bufferedWriter.newLine();
-//
-//        bufferedReader.close();
-//        bufferedWriter.close();
-
+        bufferedReader.close();
     }
 }
